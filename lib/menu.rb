49b757c7026676d5c7db15338c7792fa5133644b
sanjsanj <sanjsanj@hotmail.com>
2015-07-26 09:13:59 +0100

    changed menu items to dishes, each dish has a price

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,5 +1,9 @@
 class Menu
-  def items
-    'dummy item'
+  def dishes
+    {
+      "Fish": 4.00,
+      "Chips": 3.00,
+      "Drink": 2.00
+    }
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,7 +1,13 @@
 require 'menu'
 
 describe Menu do
-  it 'has items when created' do
-    expect(subject.items).not_to be_empty
+  it 'has dishes' do
+    expect(subject.dishes).not_to be_empty
+  end
+
+  it 'each dish has a price as a float' do
+    subject.dishes.each do |dish, price|
+      expect(subject.dishes[dish].class).to be Float
+    end
   end
 end
