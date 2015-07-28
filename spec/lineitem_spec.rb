c1d81841a22650a4e1d3779c8123d66c63b1a6cd
sanjsanj <sanjsanj@hotmail.com>
2015-07-28 20:29:51 +0100

    set up lineitem tests

--- spec/lineitem_spec.rb
+++ spec/lineitem_spec.rb
@@ -1,26 +1,30 @@
 require 'lineitem'
 
 describe Lineitem do
+  lineitem_1 = Lineitem.new "Fish"
+  lineitem_2 = Lineitem.new "Chips", 2
 
-  it 'knows which dish it has' do
-    lineitem_1 = Lineitem.new "Fish"
+  it 'knows the name of a dish' do
     expect(lineitem_1.dish).to eq "Fish"
   end
 
   it 'knows the price for each dish' do
-
+    expect(lineitem_1.price).to eq 4
   end
 
-  it 'knows the line total for quantity 1' do
-
+  it 'knows the quantity for a single quantity dish' do
+    expect(lineitem_1.qty).to eq 1
   end
 
-  it 'knows the line total for quantity 1' do
-
+  it 'knows the quantity for a multiple quantity dish' do
+    expect(lineitem_2.qty).to eq 2
   end
 
-  it 'can calculate the subtotal for a multiple quantity dish' do
-    lineitem_2 = Lineitem.new "Chips", 2
-    expect(lineitem_2.view).to eq ""
+  it 'knows the line total for a single quantity dish' do
+    expect(lineitem_1.qty).to eq 4
+  end
+
+  it 'knows the line total for a multiple quantity dish' do
+    expect(lineitem_2.line_total).to eq 6
   end
 end
