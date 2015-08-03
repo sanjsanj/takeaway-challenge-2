77e536255021468b9473e11c00f9bb62b88b0c3e
sanjsanj <sanjsanj@hotmail.com>
2015-08-03 17:12:14 +0100

    start styling for item containers

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,21 +1,17 @@
 require 'order'
 
-feature 'A customer' do
-  menu = Menu.new
-  lineitem_1 = Lineitem.new menu.dishes[0]
-  lineitem_2 = Lineitem.new menu.dishes[1], 2
-  order_1 = Order.new lineitem_1, lineitem_2
-
-  scenario 'can see a list of dishes with prices' do
+feature 'Interface test' do
+  before do
     visit '/'
-    expect(page).to have_content "Fish £4.00 Chips £3.00 Cola £2.00"
   end
 
-  scenario 'can select some number of several available dishes' do
-    expect { order_1.line_items_array }.not_to raise_error
+  scenario 'Customer can see a list of dishes with prices' do
+    expect(page).to have_content 'Fish £4.00 Chips £3.00 Cola £2.00'
   end
 
-  scenario 'can view the total of their order' do
-    expect(order_1.total).to eq 10.00
+  scenario 'Customer can select some number of several available dishes' do
+  end
+
+  scenario 'Customer can view the total of their order' do
   end
 end
